9c0366cbaf6734fc5893f3584afeb4ea132c2355
Jiang <1819188613@qq.com>
2020-12-11 17:58:53 +0800

    初始化

---
+++ Activiti7/src/main/java/com/demo/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.demo.entity.UserEntity;
+
+/**
+ * @version 1.0
+ * @Author JY
+ * @Date 2020/12/11 16:28
+ */
+public interface UserMapper extends BaseMapper<UserEntity> {
+}
